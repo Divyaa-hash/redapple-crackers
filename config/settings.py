8090833c55c428a42a6a2ea644513f730f022c33
Divyaa-hash <divyaalwar12@gmail.com>
2026-08-26 16:38:51 +0530

    Move migrations to start command to ensure database tables are created

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-j(vxrdrd#b4ayg#*tc8hi#^l^74-%#3%m$3ugrb^2kn!)datqk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # Temporarily force True for debugging
+DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 't', 'yes', 'y')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
