e5beb81a87e943cffb4a8b6ddf36f1f4bfbaa9c5
Divyaa-hash <divyaalwar12@gmail.com>
2026-08-27 19:23:18 +0530

    Serve media files in production - remove DEBUG condition for MEDIA_URL

--- config/urls.py
+++ config/urls.py
@@ -149,6 +149,8 @@ urlpatterns = [
     path('account/', TemplateView.as_view(template_name='account.html'), name='account'),
 ]
 
+# Serve media files in both development and production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
