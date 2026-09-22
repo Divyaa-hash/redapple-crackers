d456cf70b7c4277f67690db232db3bd3e8d34548
Divyaa-hash <divyaalwar12@gmail.com>
2026-09-22 19:42:42 +0530

    Remove unwanted files: copy files, PDFs, old scripts, and temporary files

--- list_static_images.py
+++
@@ -1,9 +0,0 @@
-import os
-
-static_dir = 'static/images/crackers'
-if os.path.exists(static_dir):
-    images = os.listdir(static_dir)
-    print(f'Total static images: {len(images)}')
-    print('\nFirst 20 images:')
-    for img in sorted(images)[:20]:
-        print(f'  - {img}')
